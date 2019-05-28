d6180e61fbb5ed4cabfe3c6625b024d3b1d2f7ba
sariha.mailsamy <sariha.mailsamy@srsconsultinginc.com>
2019-05-28 17:57:33 +0530

    view model added

--- src/main/webapp/app/entities/candidate/candidate-detail.component.ts
+++ src/main/webapp/app/entities/candidate/candidate-detail.component.ts
@@ -3,13 +3,22 @@ import { Router, ActivatedRoute } from '@angular/router';
 
 import { ICandidate } from 'app/shared/model/candidate.model';
 import { InterviewService } from '../interview/interview.service';
+import { IInterview } from 'app/shared/model/interview.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
     selector: 'jhi-candidate-detail',
     templateUrl: './candidate-detail.component.html'
 })
 export class CandidateDetailComponent implements OnInit {
     candidate: ICandidate;
-    constructor(protected activatedRoute: ActivatedRoute, public interviewService: InterviewService) {}
+    interview: IInterview;
+    constructor(
+        protected router: Router,
+        private modalService: NgbModal,
+        protected activatedRoute: ActivatedRoute,
+        public interviewService: InterviewService
+    ) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ candidate }) => {
@@ -20,5 +29,15 @@ export class CandidateDetailComponent implements OnInit {
 
     previousState() {
         window.history.back();
+    }
+
+    openLg(content, interview) {
+        this.modalService.open(content, { size: 'lg' });
+        this.interview = interview;
+    }
+
+    editInterview() {
+        this.router.navigate(['/interview', this.interview.id, 'view']);
+        this.modalService.dismissAll();
     }
 }
